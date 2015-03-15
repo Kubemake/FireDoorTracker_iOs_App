36b5dcd83e891af6410d943b07c2c6a74aed5313
Dmitriy Bagrov <dbagrov@bekitzur.com>
2015-03-15 21:56:20 +0300

    -added alphabet view

---
+++ firedoortracker/Controllers/ResourcesViewController/View/AlphabetView.h
@@ -0,0 +1,15 @@
+//
+//  AlphabetView.h
+//  firedoortracker
+//
+//  Created by Dmitriy Bagrov on 15.03.15.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AlphabetView : UIView
+
+- (void)displayActiveLetters:(NSArray *)activeLetters;
+
+@end
---
+++ firedoortracker/Controllers/ResourcesViewController/View/AlphabetView.m
@@ -0,0 +1,33 @@
+//
+//  AlphabetView.m
+//  firedoortracker
+//
+//  Created by Dmitriy Bagrov on 15.03.15.
+//
+//
+
+#import "AlphabetView.h"
+
+@interface AlphabetView()
+
+@property (nonatomic, strong) NSArray* alphabetButtons;
+
+@end
+
+@implementation AlphabetView
+
+#pragma mark - Display Methods
+#pragma mark -
+
+- (void)awakeFromNib {
+    
+}
+
+- (void)displayActiveLetters:(NSArray *)activeLetters {
+    
+}
+
+#pragma mark - Alphabet Buttons Generation
+#pragma mark -
+
+@end
