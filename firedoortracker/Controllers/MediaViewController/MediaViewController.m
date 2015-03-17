4dd1e00090264d520e9afdf566d6eb95e64a3b6b
Dmitriy Bagrov <dbagrov@bekitzur.com>
2015-03-17 14:06:53 +0300

    -added image picker

--- firedoortracker/Controllers/MediaViewController/MediaViewController.m
+++ firedoortracker/Controllers/MediaViewController/MediaViewController.m
@@ -8,6 +8,76 @@
 
 #import "MediaViewController.h"
 
+@interface MediaViewController() <UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+//IBOutlets
+@property (weak, nonatomic) IBOutlet UIButton *photoButton;
+@property (weak, nonatomic) IBOutlet UIButton *galleryButton;
+@property (weak, nonatomic) IBOutlet UIButton *scanQRCodeButton;
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;
+@property (weak, nonatomic) IBOutlet UITextView *descriptionTextView;
+
+@end
+
 @implementation MediaViewController
+
+#pragma mark - View Controller Lyfecircle
+#pragma mark -
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+#pragma mark - Delegate Methods
+#pragma mark - UITextFieldDelegate
+
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    //TODO: If first editing - delete text
+    textView.text = nil;
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    
+}
+
+#pragma mark - UIImagePickerDelegate
+
+- (void)imagePickerController:(UIImagePickerController *)picker
+didFinishPickingMediaWithInfo:(NSDictionary *)info {
+    UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
+    self.photoImageView.image = chosenImage;
+    
+    [picker dismissViewControllerAnimated:YES completion:nil];
+
+}
+
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+    [picker dismissViewControllerAnimated:YES completion:nil];
+}
+
+#pragma mark - IBActions
+#pragma mark - Actions from buttons
+
+- (IBAction)photoButtonPressed:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypeCamera;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (IBAction)galleryButtonPressed:(id)sender {
+    UIImagePickerController *picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.allowsEditing = YES;
+    picker.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    
+    [self presentViewController:picker animated:YES completion:nil];
+}
+
+- (IBAction)scanQRCodePressed:(id)sender {
+}
 
 @end
