dcfd0921b1c790a8137cb874e0c795b097b99a97
Nikolay Palamar <nicholas.palamar@yandex.ru>
2015-03-20 12:56:41 +0200

    Add Settings UI

--- firedoortracker/Controllers/SettingViewController/SettingViewController.m
+++ firedoortracker/Controllers/SettingViewController/SettingViewController.m
@@ -7,7 +7,58 @@
 //
 
 #import "SettingViewController.h"
+#import "UIColor+additionalInitializers.h"
+
+@interface SettingViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *containerView;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNoTextField;
+@property (weak, nonatomic) IBOutlet UITextField *emailTextField;
+
+@property (weak, nonatomic) IBOutlet UITextField *lNewPasswordTextField;
+@property (weak, nonatomic) IBOutlet UITextField *confirmPasswordTextField;
+
+@end
 
 @implementation SettingViewController
+
+#pragma mark - Lifecycle
+#pragma mark -
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    [self setupApearance];
+    [self loadData];
+}
+
+#pragma makr - Actions
+#pragma mark -
+
+- (IBAction)saveButtonPressed:(id)sender
+{
+    
+}
+
+#pragma mark - Private Logic
+#pragma mark -
+
+- (void)loadData
+{
+    
+}
+
+- (void)setupApearance
+{
+    self.title = @"SETTINGS";
+    self.containerView.layer.borderColor = [[UIColor colorOpaqueWithUCharWhite:241] CGColor]; 
+}
+
+#pragma mark - Utilities
+#pragma mark -
 
 @end
