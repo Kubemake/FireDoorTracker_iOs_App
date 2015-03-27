a65dde6480fac0d69ae48355e215d0a836d66908
Nikolay Palamar <nicholas.palamar@yandex.ru>
2015-03-27 12:36:12 +0200

    Add QR Reader to media with clipboard copyng

--- firedoortracker/Controllers/MediaViewController/MediaViewController.m
+++ firedoortracker/Controllers/MediaViewController/MediaViewController.m
@@ -6,28 +6,21 @@
 //
 //
 
-//Import Controllers
 #import "MediaViewController.h"
 #import "DoorReviewViewController.h"
-
-//Import View
-#import <SVProgressHUD.h>
-
-//Imprt Model
 #import "NetworkManager.h"
+#import <QRCodeReaderViewController.h>
+#import <SVProgressHUD.h>
+#import <NPAlertViewHelper.h>
 
 static NSString* inspectionSelectorID = @"DoorReviewViewController";
-
+static NSString* kAperture = @"aperture_id";
 static NSString* kFile = @"file";
 static NSString* kFileName = @"file_name";
-static NSString* kAperture = @"aperture_id";
 
-@interface MediaViewController() <UITextFieldDelegate,
-UIImagePickerControllerDelegate,
-UINavigationControllerDelegate,
-DoorReviewSelectionProtocol>
+@interface MediaViewController() <UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate,
+                                  DoorReviewSelectionProtocol, QRCodeReaderDelegate>
 
-//IBOutlets
 @property (weak, nonatomic) IBOutlet UIButton *photoButton;
 @property (weak, nonatomic) IBOutlet UIButton *galleryButton;
 @property (weak, nonatomic) IBOutlet UIButton *scanQRCodeButton;
@@ -39,67 +32,75 @@ DoorReviewSelectionProtocol>
 
 @implementation MediaViewController
 
-#pragma mark - View Controller Lyfecircle
-#pragma mark -
-
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    
-}
-
-#pragma mark - Delegate Methods
 #pragma mark - UITextFieldDelegate
 
-- (void)textFieldDidBeginEditing:(UITextField *)textField {
-    
-}
-
-- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+- (BOOL)textFieldShouldReturn:(UITextField *)textField
+{
     [textField resignFirstResponder];
     [self submitButtomPressed:textField];
+
     return YES;
 }
 
-
 #pragma mark - UIImagePickerDelegate
 
-- (void)imagePickerController:(UIImagePickerController *)picker
-didFinishPickingMediaWithInfo:(NSDictionary *)info {
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary *)info
+{
     UIImage *chosenImage = info[UIImagePickerControllerEditedImage];
     [self displayImage:chosenImage andHideImageSelectionButtons:YES];
     
     [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker {
+- (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker
+{
     [picker dismissViewControllerAnimated:YES completion:nil];
 }
 
 #pragma mark - Inspection Selector Delegate
 
-- (void)inspectionSelected:(Inspection *)inspection
-      doorReviewController:(UIViewController *)controller {
+- (void)inspectionSelected:(Inspection *)inspection doorReviewController:(UIViewController *)controller
+{
     [self.navigationController popViewControllerAnimated:YES];
     [SVProgressHUD show];
     __weak typeof(self) welf = self;
-    [[NetworkManager sharedInstance] performRequestWithType:uploadFileRequestType
-                                                  andParams:@{kFile : self.photoImageView.image,
-                                                              kFileName : self.descriptiontextField.text,
-                                                              kAperture : inspection.apertureId}
-                                             withCompletion:^(id responseObject, NSError *error) {
-                                                 if (error) {
-                                                     [SVProgressHUD showErrorWithStatus:error.localizedDescription];
-                                                     return;
-                                                 }
-                                                 [SVProgressHUD showSuccessWithStatus:NSLocalizedString(@"File Attached to the Inspection and Sent to the Server", nil)];
-                                                 [welf displayImage:nil andHideImageSelectionButtons:NO];
-                                             }];
+   
+    NSDictionary *params = @{ kFile     : self.photoImageView.image,
+                              kFileName : self.descriptiontextField.text,
+                              kAperture : inspection.apertureId };
+    
+    [[NetworkManager sharedInstance] performRequestWithType:uploadFileRequestType andParams:params withCompletion:^(id responseObject, NSError *error)
+    {
+        if (error) {
+            [SVProgressHUD showErrorWithStatus:error.localizedDescription];
+            return;
+        }
+        [SVProgressHUD showSuccessWithStatus:NSLocalizedString(@"File Attached to the Inspection and Sent to the Server", nil)];
+        [welf displayImage:nil andHideImageSelectionButtons:NO];
+    }];
+}
+
+#pragma mark - QRCodeReader Delegate Methods
+
+- (void)reader:(QRCodeReaderViewController *)reader didScanResult:(NSString *)result
+{
+    [self dismissViewControllerAnimated:YES completion:^{
+        [[UIPasteboard generalPasteboard] setString:result];
+        NSString *message = NSLocalizedString(@"QR Information was copied to clipboard!", @"");
+        [NPAlertViewHelper showOkAlertWithTitle:nil withMessage:message presenter:self];
+    }];
+}
+
+- (void)readerDidCancel:(QRCodeReaderViewController *)reader
+{
+    [self dismissViewControllerAnimated:YES completion:NULL];
 }
 
 #pragma mark - UI Modifying methods
 #pragma mark -
 
-- (void)displayImage:(UIImage *)image andHideImageSelectionButtons:(BOOL)hideButtons {
+- (void)displayImage:(UIImage *)image andHideImageSelectionButtons:(BOOL)hideButtons
+{
     self.photoImageView.image = image;
     [self.photoImageView setHidden:(BOOL)!image];
     [self.cancelImageButton setHidden:(BOOL)!image];
@@ -122,7 +123,8 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
 #pragma mark - IBActions
 #pragma mark - Actions from buttons
 
-- (IBAction)photoButtonPressed:(id)sender {
+- (IBAction)photoButtonPressed:(id)sender
+{
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
     picker.allowsEditing = YES;
@@ -131,7 +133,8 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
     [self presentViewController:picker animated:YES completion:nil];
 }
 
-- (IBAction)galleryButtonPressed:(id)sender {
+- (IBAction)galleryButtonPressed:(id)sender
+{
     UIImagePickerController *picker = [[UIImagePickerController alloc] init];
     picker.delegate = self;
     picker.allowsEditing = YES;
@@ -140,16 +143,27 @@ didFinishPickingMediaWithInfo:(NSDictionary *)info {
     [self presentViewController:picker animated:YES completion:nil];
 }
 
-- (IBAction)scanQRCodePressed:(id)sender {
+- (IBAction)scanQRCodePressed:(id)sender
+{
+    NSArray *types = @[AVMetadataObjectTypeQRCode];
+    QRCodeReaderViewController *qrCodeReaderViewConroller = [QRCodeReaderViewController
+                                                             readerWithMetadataObjectTypes:types];
+    qrCodeReaderViewConroller.modalPresentationStyle = UIModalPresentationFormSheet;
+    qrCodeReaderViewConroller.delegate = self;
+  
+    [self presentViewController:qrCodeReaderViewConroller animated:YES completion:NULL];
 }
 
-- (IBAction)cancelImageButtonPressed:(id)sender {
+- (IBAction)cancelImageButtonPressed:(id)sender
+{
     [self displayImage:nil andHideImageSelectionButtons:NO];
 }
 
-- (IBAction)submitButtomPressed:(id)sender {
+- (IBAction)submitButtomPressed:(id)sender
+{
     if ([self validateIFilledInfo]) {
-        DoorReviewViewController *inspectionSelector = [self.storyboard instantiateViewControllerWithIdentifier:inspectionSelectorID];
+        DoorReviewViewController *inspectionSelector = [self.storyboard
+                                                        instantiateViewControllerWithIdentifier:inspectionSelectorID];
         inspectionSelector.inspectionSelectionDelegate = self;
         [self.navigationController pushViewController:inspectionSelector animated:YES];
     }
