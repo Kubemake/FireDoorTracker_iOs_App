1d53d2cd65c9955b76f97d60de3b14e11c95d794
Dmitriy Bagrov <dbagrov@bekitzur.com>
2015-03-27 10:27:43 +0300

    -changed developer account and provisions -bug fix

--- firedoortracker/Controllers/DoorReviewViewController/View/InspectionCollectionViewCell.m
+++ firedoortracker/Controllers/DoorReviewViewController/View/InspectionCollectionViewCell.m
@@ -22,10 +22,12 @@
 
 - (void)displayInspection:(Inspection *)inspection {
     self.titleLabel.text = [NSString stringWithFormat:@"Inspection %@",inspection.uid];
-    self.descriptionLabel.text = [NSString stringWithFormat:@"%@\n%@\n%@",
+    self.descriptionLabel.text = [NSString stringWithFormat:@"%@\n%@\n%@\n%@\n%@",
                                   inspection.buildingName,
                                   inspection.inspectionStatus,
-                                  inspection.startDate];
+                                  inspection.startDate,
+                                  inspection.locationName,
+                                  (inspection.inspector) ? : @"-"];
 }
 
 @end
